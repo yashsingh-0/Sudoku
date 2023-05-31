72efc08bbfa334f9141dec399718d62f4b0c2d03
Yash Singh <77584751+yashsingh-0@users.noreply.github.com>
2023-05-31 10:53:48 +0530

    default logic

--- game.js
+++ game.js
@@ -1,1 +1,24 @@
-console.log("Hello world!");
+const gridItem = document.querySelectorAll(".grid-item > input");
+
+// Generate Random Numbers
+const generateRandomNum = ()  => {
+    const randomNum = Math.floor(Math.random() * 9) + 1;
+    return randomNum;
+}
+
+// Changing Values of Random Input Values
+
+const injectingValuesToInput = () => {
+    for(i = 0; i <= 30; i++) {
+        randomNum = generateRandomNum();
+        const randPosition = Math.floor(Math.random() * 80) + 1;
+        gridItem[randPosition].value = randomNum;
+        // for(j=0; j<=81; j++) {
+        //     if(gridItem[j] == gridItem[j+1]) {
+
+        //     }
+        // }
+    }
+    
+}
+injectingValuesToInput();
