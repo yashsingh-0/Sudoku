ce056faf2c9cdbfcb238d6fb0f7a149ed62fa086
Yash Singh <77584751+yashsingh-0@users.noreply.github.com>
2023-05-31 14:53:49 +0530

    Update game.js

--- game.js
+++ game.js
@@ -6,19 +6,32 @@ const generateRandomNum = ()  => {
     return randomNum;
 }
 
-// Changing Values of Random Input Values
+const checkingSmallSuqare = () => {
+    let sumOfSmallSquares = 0;
+    randomNum = generateRandomNum();
+    for( let i = 0; i <= 8; i++) {
+        sumOfSmallSquares += gridItem[i].value;
+        // Check if Duplicate Values Exist in small squares and replace
+        if(gridItem[i].value != "") {
+            let valuesMap = new Map();
+            valuesMap.set(i,gridItem[i].value);
+            console.log(valuesMap.get(i));
 
+            // if(gridItem[i].value == gridItem[i+1].value) {
+            //     gridItem[i].value = randomNum;
+            //     console.log(gridItem[i].value);
+            // } 
+        }       
+    }
+}
+// Changing Values of Random Input Values
 const injectingValuesToInput = () => {
     for(i = 0; i <= 30; i++) {
         randomNum = generateRandomNum();
         const randPosition = Math.floor(Math.random() * 80) + 1;
         gridItem[randPosition].value = randomNum;
-        // for(j=0; j<=81; j++) {
-        //     if(gridItem[j] == gridItem[j+1]) {
-
-        //     }
-        // }
     }
-    
+    checkingSmallSuqare();
 }
+
 injectingValuesToInput();
